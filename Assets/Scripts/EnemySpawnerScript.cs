b11e97677c9889dc9e21d2e7363f875cd7f0c504
saiIsDeveloping <saiwasherebusiness@gmail.com>
2024-08-17 17:18:36 -0700

    This is basic enemy spawner

---
+++ Assets/Scripts/EnemySpawnerScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemyPrefab;
+
+    [SerializeField]
+    private float minimumSpawnTime;
+
+    [SerializeField]
+    private float maximumSpawnTime;
+
+    private float timeUntilSpawn;
+
+    void Awake()
+    {
+        SetTimeUntilSpawn();
+    }
+
+    void Update()
+    {
+        timeUntilSpawn -= Time.deltaTime;
+
+        if (timeUntilSpawn <= 0)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            SetTimeUntilSpawn();
+        }
+    }
+
+    private void SetTimeUntilSpawn()
+    {
+        timeUntilSpawn = Random.Range(minimumSpawnTime, maximumSpawnTime);
+    }
+}
