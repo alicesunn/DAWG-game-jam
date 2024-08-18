ecc6dc4189d6d1de036b894c9c31b2d16343a95e
unknown <liujennifer6@gmail.com>
2024-08-17 22:41:38 -0400

    chick animations

--- Assets/Scripts/ChickAnimation.cs
+++ Assets/Scripts/ChickAnimation.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class ChickAnimation : MonoBehaviour
 {
+    // References
+    Animator am;
+    ChickScript cm;
+    SpriteRenderer sr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        am = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+        cm = GetComponent<ChickScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (cm.isMoving()) {
+            am.SetBool("Move", true);
+            SpriteDirectionCheck();
+        }
+        else {
+            am.SetBool("Move", false);
+        }
     }
+
+    void SpriteDirectionCheck() {
+        if (cm.direction.x < 0) {
+            sr.flipX = true;
+        }
+        else {
+            sr.flipX = false;
+        }
+    }
+
 }
--- Assets/Scripts/ChickScript.cs
+++ Assets/Scripts/ChickScript.cs
@@ -9,6 +9,7 @@ public class ChickScript : MonoBehaviour
 {
     public float speed = 4.0f; // default speed
     public float constraintRadius = 1.5f; // maintain this distance on move/stop
+    public Vector2 direction = new(0.0f, 0.0f);
 
     public GameObject parent; // bird to follow
     public GameObject child; // bird that follows
@@ -18,6 +19,7 @@ public class ChickScript : MonoBehaviour
     private float fastSpeed;
     private Rigidbody2D body;
     private StateScript state;
+    private bool moving;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,7 @@ public class ChickScript : MonoBehaviour
         normSpeed = speed;
         slowSpeed = speed * 0.5f;
         fastSpeed = speed * 1.3f;
+        moving = false;
     }
 
     // Update is called once per frame
@@ -39,12 +42,13 @@ public class ChickScript : MonoBehaviour
     private void MoveTowardsParent()
     {
         float dist = Vector2.Distance(parent.transform.position, body.transform.position);
-        Vector2 direction = (parent.transform.position - body.transform.position).normalized;
+        direction = (parent.transform.position - body.transform.position).normalized;
 
         // We want the baby to lag behind a little when it first starts moving,
         // then adjust speed based on distance from parent
         if (body.velocity == Vector2.zero) // Initial standstill
         {
+            moving = false;
             // Do not move until parent has gotten far enough away
             if (dist > constraintRadius * 1.2f)
             {
@@ -53,6 +57,7 @@ public class ChickScript : MonoBehaviour
         }
         else // While moving
         {
+            moving = true;
             float stopConstraint = (parent != state.player) ? (constraintRadius * 0.7f) : (constraintRadius * 0.9f);
 
             if (dist > constraintRadius) speed = fastSpeed; // constant speed when running
@@ -60,5 +65,9 @@ public class ChickScript : MonoBehaviour
         }
 
         body.velocity = direction * speed;
+    }
+
+    public bool isMoving() {
+        return moving;
     }
 }
